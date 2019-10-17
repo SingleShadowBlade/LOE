6869cb521726c3e052c4feab87b9254e707f1a77
gujinliang <1573483880@qq.com>
2019-10-17 19:52:06 +0800

    #1

---
+++ Assets/GameMain.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMain : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is c alled once per frame
+    void Update()
+    {
+        
+    }
+}
