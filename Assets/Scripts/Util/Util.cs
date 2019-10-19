c3a47f74e20326e8301035a5f8f7a2b628807716
gujinliang <1573483880@qq.com>
2019-10-19 17:12:09 +0800

    ProtolBuffer 工具类

---
+++ Assets/Scripts/Util/Util.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using ProtoBuf.Meta;
+
+public static class Util 
+{
+    #region 
+
+    private static ProtobufSerializer protobufSerializer = new ProtobufSerializer();
+
+    public static string PersistPath
+    {
+        get
+        {
+#if UNITY_EDITOR
+            return Application.persistentDataPath + "/StreamingAssets/PC/Assets/";
+#elif UNITY_ANDROID
+                return Application.persistentDataPath + "/StreamingAssets/Android/Assets/";
+#elif UNITY_IPHONE
+                return Application.persistentDataPath + "/StreamingAssets/iOS/Assets/";
+#else
+                return Application.persistentDataPath + "/StreamingAssets/PC/Assets/";
+#endif
+        }
+    }
+
+    #endregion
+
+    #region
+    public static void WriteBytesToFile(string pbName, byte[] bytes)
+    {
+        string localPath = PersistPath + pbName;
+        if (!File.Exists(localPath))
+            File.Create(localPath).Dispose();
+        File.WriteAllBytes(localPath, bytes);
+    }
+
+    public static void WritePBToFile<T>(T PB)
+    {
+        byte[] byteArr = SerializeProtocol(PB);
+        WriteBytesToFile(typeof(T).Name, byteArr);
+    }
+
+    public static T ReadPBFromFile<T>(string pbName)
+    {
+        string localPath = PersistPath + pbName;
+        byte[] byteArr = File.ReadAllBytes(localPath);
+        return DeserializeProtocol<T>(byteArr);
+    }
+
+    public static byte[] SerializeProtocol<T>(T kPB)
+    {
+        MemoryStream kStream = new MemoryStream();
+
+        try
+        {
+            protobufSerializer.Serialize(kStream, kPB);
+            return kStream.ToArray();
+        }
+        catch
+        {
+            Debug.LogError("Can't Encode PB " + typeof(T).Name);
+            return null;
+        }
+    }
+
+    public static T DeserializeProtocol<T>(byte[] kbyte)
+    {
+        try
+        {
+            T kProtocol = default(T);
+            using (MemoryStream kStream = new MemoryStream(kbyte))
+            {
+                kProtocol = (T)protobufSerializer.Deserialize(kStream, null, typeof(T));
+            }
+            return kProtocol;
+        }
+        catch
+        {
+            Debug.LogError("Can't Decode PB " + typeof(T).Name);
+            return default(T);
+        }
+    }
+    #endregion
+}
--- Tools/ProtolBuffer/GenCs.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set tool=ProtoGen
- 
-set proto=test.proto
- 
-%tool%\protogen.exe -i:%proto% -o:%proto%.cs -ns:Proto
- 
-pause
